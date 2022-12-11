057a84655f35c0b8d5c6528b58fbd5e5874ff1e3
GabrielleMuniz <gabimusants02@gmail.com>
2022-12-11 20:40:04 -0300

    implem. semáforo

--- so.c
+++ so.c
@@ -1,39 +1,78 @@
 #include <stdio.h> 
-#include <stdlib.h> 
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/shm.h>
+#include <sys/msg.h>
 #include <pthread.h>
+#include <semaphore.h>
 
-void *altVar(int *vars) {
+sem_t sem;                                                      // Declaração do semáforo
+
+void *altVarThreadA(int *vars) {
     int i = 0, v0;
     int numSerie = 1;
-    v0 = vars[0];                                              // Copia o valor da 1ª variável da shm para uma variável local
 
-    printf("Id da thread: %ld\n\n", pthread_self());
+    printf("Id da thread A: %ld\n\n", pthread_self());
 
-    for (i; i < 5; i++) {
+    for (i; i < 100; i++) {
+        sem_wait(&sem);                                         // Tenta entrar na região crítica
+        v0 = vars[0];                                           // Copia o valor da 1ª variável da shm para uma variável local
         v0--;                                                   // Decrementa o valor da variável copiada
-        sleep(1);                                               // Dorme um tempo aleatório
-        vars[0] = v0;                                          // Armazena o valor decrementado na 1ª variável da memória compartilhada
+        sleep(0.4);                                             // Dorme um tempo aleatório
+        vars[0] = v0;                                           // Armazena o valor decrementado na 1ª variável da memória compartilhada
         vars[1]++;
         
         // Exibe na tela informações relevantes
-        printf("Numero de serie: %d\n", numSerie);
+        printf("Numero de serie (thread A): %d\n", numSerie);
         numSerie++;
         printf("Variavel 1 = %d\n", vars[0]);
         printf("Variavel 2 = %d\n", vars[1]);
-        printf("\n-----------------------------\n\n");
-        sleep(0.1);                                               // Dorme um tempo aleatório
+        printf("\n------------------------------\n\n");
+        sem_post(&sem);                                         // Libera a região crítica
+        sleep(1);                                               // Dorme um tempo aleatório
     }
+
+    printf("Thread A terminou!\n");
+    printf("\n++++++++++++++++++++++++++++++\n\n");
 }
 
+void *altVarThreadB(int *vars) {
+    int i = 0, v0;
+    int numSerie = 1;
+
+    printf("Id da thread B: %ld\n\n", pthread_self());
+
+    for (i; i < 100; i++) {
+        sem_wait(&sem);                                         // Tenta entrar na região crítica
+        v0 = vars[0];                                           // Copia o valor da 1ª variável da shm para uma variável local
+        v0--;                                                   // Decrementa o valor da variável copiada
+        sleep(0.6);                                             // Dorme um tempo aleatório
+        vars[0] = v0;                                           // Armazena o valor decrementado na 1ª variável da memória compartilhada
+        vars[1]++;
+
+        // Exibe na tela informações relevantes
+        printf("Numero de serie (thread B): %d\n", numSerie);
+        numSerie++;
+        printf("Variavel 1 = %d\n", vars[0]);
+        printf("Variavel 2 = %d\n", vars[1]);
+        printf("\n------------------------------\n\n");
+        sem_post(&sem); // Libera a região crítica
+        sleep(1);       // Dorme um tempo aleatório
+    }
+
+    printf("Thread B terminou!\n");
+    printf("\n++++++++++++++++++++++++++++++\n\n");
+}
 
 int main() {
-    int *vars;                                          // Variável inicializada com valor 300
-    
+    int *vars;                                                  // Variável inicializada com valor 300
+
+    sem_init(&sem, 0, 1);                                       // Inicialização do semáforo
+
     int shm_Id = shmget(IPC_PRIVATE,
                         sizeof(int) * 2,
-                        SHM_R | SHM_W | IPC_CREAT);     // Cria a região de memória compartilhada dentro do processo pai
+                        SHM_R | SHM_W | IPC_CREAT);             // Cria a região de memória compartilhada
 
     if (shm_Id < 0) {
         printf("Ocorreu um erro ao criar a area de memoria compartilhada!\n");
@@ -42,97 +81,79 @@ int main() {
     
     int id = fork();
 
-    if(id > 0) {                                           // id > de 0 é o processo pai
+    if(id > 0) {                                                // id > de 0 é o processo pai
         int numSerie = 1;
 
-        printf("\n+++++++++++++++++++++++++++++\n\n");
+        printf("\n++++++++++++++++++++++++++++++\n\n");
         printf("Main - ID: %d\n", getpid());
         printf("Numero de serie: %d\n", numSerie);
         numSerie++;
 
-        vars = (int *) shmat(shm_Id, NULL, 0);           // Anexa a variável à região de memória
+        vars = (int *) shmat(shm_Id, NULL, 0);                  // Anexa a variável à região de memória
 
         vars[0] = 300;
         vars[1] = 0;
         
         printf("Variavel 1 = %d\n", vars[0]);
         printf("Variavel 2 = %d\n", vars[1]);
-        printf("\n+++++++++++++++++++++++++++++\n\n");
+        printf("\n++++++++++++++++++++++++++++++\n\n");
 
         int id_2 = fork();
 
-        if(id_2 == 0) {                                 // Filho 2 do main
-            sleep(5);
+        if(id_2 == 0) {                                         // Filho 2 do main
+            //sleep(5);
             int i, v0;
             int numSerie = 1;
 
             printf("Sou filho 2 - ID: %d\n", getpid());
-            //printf("Numero de serie: %d\n", numSerie);
-            //numSerie++;
 
-            vars = (int *)shmat(shm_Id, NULL, 0);       // Anexa a variável à região de memória
-            v0 = vars[0];
-            //vars[0] = 200;
-            //vars[1] = 10;
+            vars = (int *)shmat(shm_Id, NULL, 0);               // Anexa a variável à região de memória
 
-            for (i; i < 5; i++) {
-                v0--;         // Decrementa o valor da variável copiada
-                sleep(1);     // Dorme um tempo aleatório
-                vars[0] = v0; // Armazena o valor decrementado na 1ª variável da memória compartilhada
+            for (i; i < 100; i++) {
+                sem_wait(&sem);                                 // Tenta entrar na região crítica
+                v0 = vars[0];
+                v0--;                                           // Decrementa o valor da variável copiada
+                sleep(0.8);                                     // Dorme um tempo aleatório
+                vars[0] = v0;                                   // Armazena o valor decrementado na 1ª variável da memória compartilhada
                 vars[1]++;
 
                 // Exibe na tela informações relevantes
-                printf("Numero de serie: %d\n", numSerie);
+                printf("Numero de serie (filho 2): %d\n", numSerie);
                 numSerie++;
                 printf("Variavel 1 = %d\n", vars[0]);
                 printf("Variavel 2 = %d\n", vars[1]);
-                printf("\n-----------------------------\n\n");
-                sleep(0.1); // Dorme um tempo aleatório
+                printf("\n------------------------------\n\n");
+                sem_post(&sem);                                 // Libera a região crítica
+                sleep(1);                                       // Dorme um tempo aleatório
             }
 
-            //printf("Var 1 = %d\n", vars[0]);
-            //printf("Var 2 = %d\n", vars[1]);
+            printf("Filho 2 terminou!\n"); 
+            printf("\n++++++++++++++++++++++++++++++\n\n");
 
-            // APAGAR ESTE PRINT DEPOIS QUE TIVER FEITO A TROCA DE MENSAGEM
-            printf("Filho 2 terminou!\n"); // APAGAR ESTE PRINT DEPOIS QUE TIVER FEITO A TROCA DE MENSAGEM
-            printf("\n+++++++++++++++++++++++++++++\n\n");
-
-            shmdt(vars);                                // Desanexa região de memória da variável 1
+            shmdt(vars);                                        // Desanexa região de memória da variável 1
 
         }
         
-        //sleep(20);
-        while((vars[0] != 285) && (vars[1] != 15));
-        //while(*var2 != 300);
+        while((vars[0] != 0) && (vars[1] != 300));
+        sleep(10);
+        shmdt(vars);                                            // Desanexa região de memória da variável 1
 
-        shmdt(vars);                                    // Desanexa região de memória da variável 1
-        //shmdt(var2);                                    // Desanexa região de memória da variável 2
+        shmctl(shm_Id, IPC_RMID, NULL);                         // Destrói a região de memória compartilhada
 
-        shmctl(shm_Id, IPC_RMID, NULL);                 // Destrói a região de memória compartilhada
+    } else if (id == 0){                                        // id == 0 é o processo filho, filho 1 do main
+        printf("Sou filho 1 - ID: %d\n", getpid());
 
-    } else if (id == 0){                                // id == 0 é o processo filho, filho 1 do main
-        sleep(12);
-
-        printf("Sou filho 1 - ID: %d\n\n", getpid());
-
-        vars = (int *)shmat(shm_Id, NULL, 0);                 // Anexa a variável à região de memória
-        //var2 = (int *)shmat(shm_Id, NULL, 0);               // Anexa a variável à região de memória
-        //vars[0] = 200;
-        //vars[1] = 20;
+        vars = (int *)shmat(shm_Id, NULL, 0);                   // Anexa a variável à região de memória
 
         pthread_t thrA, thrB;
 
-        pthread_create(&thrA, NULL, (void *)altVar, vars); // Cria thread com id, inic, função e parâmetro da função
-        pthread_join(thrA, NULL);                           // Espera fim das threads
-        printf("Thread A terminou!\n");
-        printf("\n+++++++++++++++++++++++++++++\n\n");
+        pthread_create(&thrA, NULL, (void *)altVarThreadA, vars); // Cria thread com id, inic, função e parâmetro da função
+        pthread_create(&thrB, NULL, (void *)altVarThreadB, vars); // Cria thread com id, inic, função e parâmetro da função
 
-        pthread_create(&thrB, NULL, (void *)altVar, vars); // Cria thread com id, inic, função e parâmetro da função
-        pthread_join(thrB, NULL);                           // Espera fim das threads
-        printf("Thread B terminou!\n");
-        printf("\n+++++++++++++++++++++++++++++\n\n");
+        pthread_join(thrA, NULL);                               // Espera fim das threads
+        pthread_join(thrB, NULL);                               // Espera fim das threads
 
-        shmdt(vars);                                    // Desanexa região de memória da variável 1
+        shmdt(vars);                                            // Desanexa região de memória da variável 1
 
     } else {
         printf("Fork falhou!\n");
